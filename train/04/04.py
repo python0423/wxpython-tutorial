15329e40d1bd879897d964a375a50e912bbcad36
python0423 <weilaidegeng1994@163.com>
2017-08-20 21:38:06 +0800

    temp

---
+++ train/04/04.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author: admin
+# @Date:   2017-08-20 21:29:34
+# @Last Modified by:   admin
+# @Last Modified time: 2017-08-20 21:36:23
+import wx
+class ClassName(object):
+	"""docstring for ClassName"""
+	def __init__(self, arg):
+		super(ClassName, self).__init__()
+		self.arg = arg
+		
